585d9269011077256d191c9c5291aa3a3ff6f871
Alexander Yanin <472709@edu.itmo.ru>
2025-02-28 13:59:55 +0300

    Implement token printing in printer-java

--- tool/printer-java/src/main/java/libslprinter/Main.java
+++ tool/printer-java/src/main/java/libslprinter/Main.java
@@ -8,13 +8,24 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length != 2) {
             printUsage();
             System.exit(1);
         }
 
+        var mode = switch (args[0].toLowerCase()) {
+            case "tokens" -> Mode.TOKENS;
+            case "parse-tree" -> Mode.PARSE_TREE;
+            default -> {
+                System.err.println("Invalid command: " + args[1]);
+                printUsage();
+                System.exit(1);
+                throw new RuntimeException("unreachable");
+            }
+        };
+
         try {
-            new Main().run(args[0]);
+            new Main().run(args[1], mode);
         } catch (Exception e) {
             System.err.printf("Encountered a failure: %s!\n", e);
             e.printStackTrace(System.err);
@@ -23,12 +34,43 @@ public class Main {
     }
 
     private static void printUsage() {
-        System.err.println("Usage: java -jar printer-java.jar <path to source file>");
+        System.err.println("Usage: java -jar printer-java.jar {tokens|parse-tree} <path to source file>");
     }
 
-    private void run(String path) throws IOException {
+    private void run(String path, Mode mode) throws IOException {
         var stream = CharStreams.fromFileName(path, StandardCharsets.UTF_8);
         var lexer = new LibSLLexer(stream);
+
+        if (mode == Mode.TOKENS) {
+            printTokens(lexer);
+            return;
+        }
+
+        printParseTree(lexer);
+    }
+
+    private void printTokens(LibSLLexer lexer) {
+        for (int idx = 0;; ++idx) {
+            var token = lexer.nextToken();
+            System.out.printf(
+                "Token %d: channel %s, type %s, %s\n",
+                idx,
+                getName(lexer.getChannelNames(), token.getChannel()),
+                lexer.getVocabulary().getSymbolicName(token.getType()),
+                token
+            );
+
+            if (token.getType() == LibSLLexer.EOF) {
+                break;
+            }
+        }
+    }
+
+    private static String getName(String[] names, int idx) {
+        return 0 <= idx && idx < names.length ? names[idx] : "[unknown]";
+    }
+
+    private void printParseTree(LibSLLexer lexer) {
         var tokens = new CommonTokenStream(lexer);
         var parser = new LibSLParser(tokens);
         var printer = new ParseTreePrinter();
@@ -36,5 +78,10 @@ public class Main {
         parser.addParseListener(printer);
         parser.removeErrorListeners();
         parser.file();
+    }
+
+    private enum Mode {
+        TOKENS,
+        PARSE_TREE,
     }
 }
